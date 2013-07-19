6491f38e0370c86494f82712f78b714984ed8d10
Ben Israeli <jisraeli@gmail.com>
2013-07-18 23:46:30 -0400

    sorcery migrated

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,70 @@
 class Player < ActiveRecord::Base
   authenticates_with_sorcery!
-  # attr_accessible :title, :body
+  attr_accessor :password
+  before_save :encrypt_password
+
+
+  include Gravtastic
+  gravtastic :filetype => :gif, :rating => "PG", :default => "identicon"
+  has_many :games
+  has_many :challengers, :through => :games, source: 'challenger'
+  attr_accessible :firstname, :lastname, :email, :height, :weight, :zipcode, :email, :password, :password_confirmation
+
+  validates :password, confirmation: true
+  validates :password, presence: true, on: :create
+  validates :firstname, :lastname, length: { in: 2..50 }
+
+  validates :firstname, :lastname, :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :height, format: { with: /\A[4-8]'[0-9][0-9]?\z/,
+    message: "should be in the form of 5'9" }
+
+  validates :weight, numericality: { in: 80..300 }
+
+  def encrypt_password
+    if password.present?
+        #This generates a random string that helps me encrypt the password.
+        self.password_salt= BCrypt::Engine.generate_salt
+        self.password_hash= BCrypt::Engine.hash_secret(password, password_salt)
+
+    end
+  end
+
+  #I can call this with User.authenticate(l,p)
+  def self.authenticate(email, password)
+    #This will auth a user
+    player = Player.find_by_email(email)
+    if player && player.password_hash == BCrypt::Engine.hash_secret(password, password_salt)
+      player
+    else
+      nil
+    end
+  end
 end
+
+  attr_accessor :password
+  before_save :encrypt_password
+
+
+  include Gravtastic
+  gravtastic :filetype => :gif, :rating => "PG", :default => "identicon"
+  has_many :games
+  has_many :challengers, :through => :games, source: 'challenger'
+  attr_accessible :firstname, :lastname, :email, :height, :weight, :zipcode, :email, :password, :password_confirmation
+
+  validates :password, confirmation: true
+  validates :password, presence: true, on: :create
+  validates :firstname, :lastname, length: { in: 2..50 }
+
+  validates :firstname, :lastname, :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :height, format: { with: /\A[4-8]'[0-9][0-9]?\z/,
+    message: "should be in the form of 5'9" }
+
+  validates :weight, numericality: { in: 80..300 }
+
+
+end
+
--- db/migrate/20130719033757_sorcery_core.rb
+++ db/migrate/20130719033757_sorcery_core.rb
@@ -1,16 +1,8 @@
 class SorceryCore < ActiveRecord::Migration
-  def self.up
-    create_table :players do |t|
-      t.string :username,         :null => false  # if you use another field as a username, for example email, you can safely remove this field.
-      t.string :email,            :default => nil # if you use this field as a username, you might want to make it :null => false.
-      t.string :crypted_password, :default => nil
-      t.string :salt,             :default => nil
+  def self.change
+    add_column :players, :crypted_password, :string, :default => :nil
+    add_column :players, :salt, :string, :default => :nil
 
-      t.timestamps
-    end
   end
 
-  def self.down
-    drop_table :players
-  end
 end
